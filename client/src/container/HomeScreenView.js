b0fe1135bef5341b2191146414bbd850319ffcf5
alexli2002 <1alexli2002@gmail.com>
2022-03-22 11:55:34 -0400

    added airbnb eslint configuration and babel to support es6

--- client/src/container/HomeScreenView.js
+++ client/src/container/HomeScreenView.js
@@ -1,12 +1,33 @@
 import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const HomeScreenView = () => {
+  const test = {
+    a: 'test',
+    c: 'test123',
+  };
+
+  const final = {
+    ...test,
+    d: 'test1235',
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Testing HomeScreen!</Text>
+      <Text>{'testing babel spread operator: ' + JSON.stringify(final)}</Text>
     </View>
   );
-}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+    padding: 20,
+    margin: 10,
+  },
+});
 
 export default HomeScreenView;
-
---
+++ server/api/server.js
@@ -0,0 +1,5 @@
+/*
+    PLACE API ROUTING CODE IN THIS FILE
+*/
+const path = require("path");
+const express = require("express");
